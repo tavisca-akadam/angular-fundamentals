35a76e24023a2267ae0de05e7ea3b8157c44461a
tavisca-akadam <ankadam@tavisca.com>
2020-05-16 21:44:16 +0530

    Added route resolve gaurd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { appRoutes } from './routes';
 import { CreateEventComponent } from './events/create-event/create-event.component';
 import { Error404Component } from './common/error/error-404.component';
 import { EventRouteActivator } from './events/events-details/event-route-activator.service';
+import { EventListResolver } from './events/event-list-resovler.service';
 @NgModule({
   declarations: [
     EventsAppComponent,
@@ -31,6 +32,7 @@ import { EventRouteActivator } from './events/events-details/event-route-activat
     EventService,
     ToastrService,
     EventRouteActivator,
+    EventListResolver,
     {
       provide: 'canDeactivateCreateEvent',
       useValue: checkDirtyState
