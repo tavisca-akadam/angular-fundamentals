bbafafc71e5d4de63e42f24ec9785f834e5f8b0d
tavisca-akadam <ankadam@tavisca.com>
2020-05-16 21:45:52 +0530

    Reloading component data using resolve gaurd

--- src/app/events/event-list.component.ts
+++ src/app/events/event-list.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
 import { ToastrService } from '../common/services/toastr.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './event-list-component.html'
 })
 export class EventsListComponent implements OnInit {
-  events: any[];
-  constructor(private eventService: EventService, private toastrService: ToastrService) { }
+  events: any;
+  constructor(private eventService: EventService, private toastrService: ToastrService, private route: ActivatedRoute) { }
   ngOnInit() {
-    this.events = this.eventService.getEvents();
+    this.events = this.route.snapshot.data['events'];
   }
   handleThumbnailClick(eventName) {
     this.toastrService.success(eventName);
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,10 +4,11 @@ import { EventsDetailsComponent } from './events/events-details/events-details.c
 import { CreateEventComponent } from './events/create-event/create-event.component';
 import { Error404Component } from './common/error/error-404.component';
 import { EventRouteActivator } from './events/events-details/event-route-activator.service';
+import { EventListResolver } from './events/event-list-resovler.service';
 
 export const appRoutes: Routes = [
     { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
-    { path: 'events', component: EventsListComponent },
+    { path: 'events', component: EventsListComponent, resolve: { events: EventListResolver } },
     { path: 'events/:id', component: EventsDetailsComponent, canActivate: [EventRouteActivator] },
     { path: '404', component: Error404Component },
     { path: '', redirectTo: '/events', pathMatch: 'full' }
