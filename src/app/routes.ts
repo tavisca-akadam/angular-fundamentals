53f434ddae5c57975a1f1530beef94a1452a3156
tavisca-akadam <ankadam@tavisca.com>
2020-05-16 19:21:08 +0530

    Added link between all events and event thumbnail componet

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,6 +4,6 @@ import { EventsDetailsComponent } from './events/events-details/events-details.c
 
 export const appRoutes: Routes = [
     { path: 'events', component: EventsListComponent },
-    { path: 'event/:id', component: EventsDetailsComponent },
+    { path: 'events/:id', component: EventsDetailsComponent },
     { path: '', redirectTo: '/events', pathMatch: 'full' }
 ]
